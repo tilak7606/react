8e49277726685e68a75dbe68446c71af686b678b
tilak7606 <tilak113saini@gmail.com>
2024-10-19 22:08:45 +0530

    some little changes

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Footer() {
     return (
@@ -36,14 +36,13 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <a
-                                        href="https://github.com/hiteshchoudhary"
-                                        className="hover:underline"
-                                        target="_blank"
-                                        rel="noreferrer"
+                                    < NavLink 
+                                        to="https://github.com/tilak7606"
+                                        className = {({isActive}) => `hover:underline ${isActive ?  "text-orange-700" : "text-gray-700" }`}
+                                       
                                     >
                                         Github
-                                    </a>
+                                    </ NavLink>
                                 </li>
                                 <li>
                                     <Link to="/" className="hover:underline">
--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -4,8 +4,8 @@ import { useParams } from 'react-router-dom'
 export default function User() {
     const {userid} = useParams();
   return (
-    <div className='flex self-center'>
-        User:{userid}
+    <div className='flex text-center justify-center bg-red-300 h-8 p-1.5 '>
+        User : {userid}
     </div>
   )
 }
